e5862354fc9b4f3468970aaeefb31018abef43a7
sirkotal <lordkotal2002@gmail.com>
2023-02-22 11:51:33 +0000

    Added some header files

--- lab2/timer.c
+++ lab2/timer.c
@@ -29,10 +29,9 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  sys_outb(TIMER_CTRL, TIMER_RB_CMD | TIMER_RB_SEL(timer) | TIMER_RB_COUNT_);
+  
+  return util_sys_inb(TIMER_0 + timer, st);
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
