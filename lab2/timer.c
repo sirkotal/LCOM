7ad111e0e00dff842e4749b2f0314f4983cf3b67
sirkotal <93833262+sirkotal@users.noreply.github.com>
2023-03-01 11:44:08 +0000

    Implemented the timer_set_frequency function

--- lab2/timer.c
+++ lab2/timer.c
@@ -9,7 +9,61 @@
 #define SUCCESS 0
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  return 1;
+  if (timer > 2) {
+    return FAIL;
+  }
+
+  uint8_t st;
+  uint16_t timer_freq = (uint16_t) (TIMER_FREQ / freq);
+
+  if (timer_get_conf(timer, &st)) {
+    return FAIL;
+  }
+
+  /*max freq is 0xFFFF for binary count, 0x9999 for BCD */
+  if (freq < TIMER_FREQ / 0xFFFF) {
+    return FAIL;
+  } 
+  else if ((st & BIT(0)) && freq < TIMER_FREQ / 0x9999) {
+    return FAIL;
+  }
+
+  if (st & BIT(0)) {
+    timer_freq = bin_to_bcd(timer_freq);
+    return FAIL;
+  }
+
+  uint8_t ctrl_timer; 
+  if (timer) {
+    ctrl_timer = BIT(5 + timer);
+  }
+  else {
+    ctrl_timer = TIMER_SEL0;
+  }
+
+  uint8_t ctrl_word = (ctrl_timer | TIMER_LSB_MSB | (st & (BIT(3) | BIT(2) | BIT(1))) | (st & BIT(0)));
+
+  if (sys_outb(TIMER_CTRL, ctrl_word)) {
+    return 1;
+  }
+
+  uint8_t freq_lsb;
+  util_get_LSB(timer_freq, &freq_lsb);
+
+  uint8_t freq_msb;
+  util_get_MSB(timer_freq, &freq_msb);
+
+  if (sys_outb(TIMER_0 + timer, freq_lsb)) {
+    return 1;
+  }
+
+  if (sys_outb(TIMER_0 + timer, freq_msb)) {
+    return 1;
+  }
+
+  return 0;
+
+
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
@@ -27,8 +81,7 @@ int (timer_unsubscribe_int)() {
 }
 
 void (timer_int_handler)() {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  return FAIL;
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
@@ -55,7 +108,7 @@ int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field fiel
         conf.byte = st;
         break;
       case tsf_initial:
-        conf.in_mode = (st & (TIMER_RB_COUNT_ | TIMER_RB_STATUS_)) >> 4;
+        conf.in_mode = (st & TIMER_LSB_MSB) >> 4;
         break;
       case tsf_mode:
         conf.count_mode = (st & (BIT(3) | BIT(2) | BIT(1))) >> 1;
